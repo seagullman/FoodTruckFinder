3025ee2a20bd0789fac0366e58c0d9f031b497f8
Brad Siegel <blsiege@gmail.com>
2024-03-05 16:31:51 -0500

    Initial Commit

---
+++ FoodTruckFinder/FoodTruckFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTruckFinderApp.swift
+//  FoodTruckFinder
+//
+//  Created by Brad Siegel on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodTruckFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
