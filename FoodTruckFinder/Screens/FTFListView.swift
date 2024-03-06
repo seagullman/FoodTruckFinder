26e8488dadd01c07fac99012c6abc2e194481cee
Brad Siegel <blsiege@gmail.com>
2024-03-05 23:07:58 -0500

    Added distance filter

--- FoodTruckFinder/Screens/FTFListView.swift
+++ FoodTruckFinder/Screens/FTFListView.swift
@@ -12,10 +12,10 @@ struct FTFListView: View {
     
     @Bindable var viewModel = ViewModel()
     @StateObject var locationManager = LocationManager() // TODO: move this to .app file? or somewhere else and store
+    @State private var distance: Double = 2.0
     
     var body: some View {
         NavigationStack {
-
             List {
                 ForEach(viewModel.foodTrucks, id: \.id) { foodTruck in
                     FoodTruckListCell(
@@ -30,11 +30,39 @@ struct FTFListView: View {
             .refreshable {
                 locationManager.refreshLocation()
             }
+            .navigationTitle("Food Trucks")
+            .toolbar {
+                Menu("Distance", systemImage: "line.3.horizontal.decrease.circle") {
+                    Picker("Distance", selection: $distance) {
+                        Text("1 mile")
+                            .tag(1.0)
+                        
+                        Text("2 miles")
+                            .tag(2.0)
+                        
+                        Text("5 miles")
+                            .tag(5.0)
+                        
+                        Text("10 miles")
+                            .tag(10.0)
+                        
+                        Text("200 miles")
+                            .tag(200.0)
+                    }
+                }
+            }
         }
         .onChange(of: locationManager.lastLocation) {
-            if let location = locationManager.lastLocation {
-                _ = viewModel.fetchFoodTrucks(200, of: location)
-            }
+            fetchFoodTrucks()
+        }
+        .onChange(of: distance) {
+            fetchFoodTrucks()
+        }
+    }
+    
+    func fetchFoodTrucks() {
+        if let location = locationManager.lastLocation {
+            _ = viewModel.fetchFoodTrucks(distance, of: location)
         }
     }
 }
